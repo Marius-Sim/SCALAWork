0c4bd9c4d5f34fcbceeed2b393973816793e2ca4
Marius <mariusnoimis@gmail.com>
2022-10-21 14:40:50 +0100

    Finished banking system

--- src/com/wiley/engageBankingSystem/Loan.scala
+++ src/com/wiley/engageBankingSystem/Loan.scala
@@ -1,5 +1,7 @@
 package com.wiley.engageBankingSystem
 
+//A loan can only be approved from the Bank skeleton interface
+//Once a loan is approved, the value that was requested gets added to the account that created the request
 class Loan(loan_id: Int, loan_amount: Double) {
   val id: Int = loan_id
   val amount: Double = loan_amount
